928221f62876bef7ce166533b6f50f926782f812
bhstarkey <116474586+bhstarkey@users.noreply.github.com>
2023-01-09 11:26:00 -0700

    Added Challenge code for deliverables 1 and 2

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def stats(start=None, end=None):
         results = session.query(*sel).\
             filter(Measurement.date >= start).all()
         temps = list(np.ravel(results))
-        return jsonify(temps=temps)
+        return jsonify(temps)
     
     results = session.query(*sel).\
         filter(Measurement.date >= start).\
